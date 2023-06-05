dd968ec80e1046e7b5f8f55e30129901d785973e
pronounceA <zyank.wpo.zyank.wpo@gmail.com>
2023-06-06 01:02:41 +0900

    add MainActivity2

--- app/src/main/java/com/example/pager2/MainActivity.kt
+++ app/src/main/java/com/example/pager2/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.pager2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.pager2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.horizontalButton.setOnClickListener { onButtonTapped(it) }
+        binding.verticalButton.setOnClickListener { onButtonTapped(it) }
+        binding.tapButton.setOnClickListener { onButtonTapped(it) }
     }
+
+    fun onButtonTapped(view: View?) {
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/pager2/MainActivity2.kt
@@ -0,0 +1,21 @@
+package com.example.pager2
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.pager2.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
